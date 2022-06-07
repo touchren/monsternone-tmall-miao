8bbfeb937e17981bc9ed885d07edebf2b046cfd7
touchren <touchren@gmail.com>
2022-06-07 21:33:54 +0800

    增加无人值守完成所有任务脚本

---
+++ jd_reward.js
@@ -0,0 +1,16 @@
+let count = 0;
+while (true && count++ < 10) {
+  text("抽奖").findOne(5000).click();
+  sleep(5000);
+  let comBtn = textMatches("(立即完成|开心收下|累计任务奖励).*").findOne(5000);
+  log(comBtn);
+  if (comBtn.text() == "累计任务奖励") {
+    break;
+  }
+  click(comBtn.bounds().centerX(), comBtn.bounds().centerY());
+  if (comBtn == "立即完成") {
+    sleep(5000);
+    back();
+  }
+  sleep(5000);
+}
---
+++ main_silent.js
@@ -0,0 +1,54 @@
+// require("./Unlock.js").exec();
+
+// let util = require("utils.js");
+
+// 在此处给脚本排队即可
+let filePathList = ["start.js", "start_jd.js", "jd_choujiang.js"];
+
+filePathList = filePathList.map(function (filePath) {
+  return files.path(filePath);
+});
+
+events.on("exit", function () {
+  log("所有任务执行完毕, 退出主进程");
+});
+
+setInterval(function () {}, 1000);
+
+let limitTime = 60 * 60 * 1000;
+
+while (1) {
+  if (filePathList.length > 0) {
+    let e = engines.execScriptFile(filePathList[0]);
+    while (!e.getEngine()); //等待脚本运行
+    let currentScriptEngine = e.getEngine();
+    let lastTime = new Date().getTime();
+    while (1) {
+      let currentTime = new Date().getTime();
+      let confirmBtn = id("md_buttonDefaultPositive").findOnce();
+      if (confirmBtn) {
+        log("检测到[确定]按钮, 等待用户自己选择");
+        sleep(10 * 1000);
+        if ((confirmBtn = id("md_buttonDefaultPositive").findOnce())) {
+          log("用户未进行选择, 自动[确定]");
+          confirmBtn.click();
+        }
+      }
+      if (currentTime - lastTime > limitTime) {
+        log("脚本运行超时, 开始 执行销毁命令");
+        currentScriptEngine.forceStop();
+        log("脚本运行超时, 结束 执行销毁命令");
+        break;
+      }
+      if (currentScriptEngine.isDestroyed()) {
+        log("脚本[%s]已退出", filePathList[0]);
+        break;
+      } else {
+        sleep(10 * 1000);
+      }
+    }
+  } else {
+    engines.myEngine().forceStop();
+  }
+  filePathList.shift();
+}
