0b7987a939791ff7429f0b41a043bfbfaa8a6ee7
MonsterNone <alwaysalittlemonster@gmail.com>
2022-11-02 08:45:25 +0800

    主会场

--- tb_huichang.js
+++ tb_huichang.js
@@ -351,7 +351,7 @@ try {
                     throw '商品页未能加载'
                 }
             }
-            sleep(1000)
+            // sleep(1000)
             backToList()
         } else if (jumpButton[0].match(/搜索/)) {
             jumpButton[1].click()
@@ -362,15 +362,17 @@ try {
                 listView.child(1).click()
             }
             liulan()
+            sleep(1000)
+            back()
             backToList()
-        } else if (jumpButton[0].match(/为你推荐/)) {
+        } else if (jumpButton[0].match(/为你推荐|主会场/)) {
             jumpButton[1].click()
             liulan()
         } else {
             jumpButton[1].click()
             liulan()
-            sleep(1000)
-            back()
+            // sleep(1000)
+            // back()
             backToList()
         }
 
